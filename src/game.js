4338c1e17dba89007b11584f40618855a630f431
Isaiah Mann <isaiahbmann@gmail.com>
2020-01-16 23:35:11 -0800

    Add game state obj. Prevent illegal moves (#3)
    * Add game state obj
    
    * Prevent illegal moves
    
    * Add turn logic
    
    * Refactor test suite

--- src/game.js
+++ src/game.js
@@ -1,27 +1,51 @@
-const allowedLetters = new Set(["x", "o"]);
+const xSymbol = "x";
+const oSymbol = "o";
+const allowedLetters = new Set([xSymbol, oSymbol]);
 const boardSize = 3;
 let board;
+let state;
 
 const start = () => {
     board = new Array(boardSize).fill(Array(boardSize).fill(null));
+    state = {
+        turn: xSymbol,
+        winner: false
+    };
 };
 
 const getBoard = () => {
     return board;
 };
 
+const getState = () => {
+    return state;
+};
+
 const inBounds = (x, y) => {
     return x >= 0 && x < boardSize && y >= 0 && y < boardSize;
 }
 
+const nextTurn = (letter) => {
+    return letter == xSymbol ? oSymbol : xSymbol;
+};
+
+const isValidMove = (letter, x, y) => {
+    return allowedLetters.has(letter) && 
+            inBounds(x, y) && 
+            board[x][y] === null &&
+            state.turn === letter;
+};
+
 const placeLetter = (letter, x, y) => {
-    if (allowedLetters.has(letter) && inBounds(x, y) && board[x][y] == null) {
+    if (isValidMove(letter, x, y)) {
         board[x][y] = letter;
+        state.turn = nextTurn(letter);
     }
 };
 
 module.exports = {
     start,
     getBoard,
+    getState,
     placeLetter
 };
--- tst/game.test.js
+++ tst/game.test.js
@@ -1,105 +1,145 @@
-it("can load the module", () => {
-    require("../src/game.js");
+describe("sanity checks", () => {
+    it("can load the module", () => {
+        require("../src/game.js");
+    });
 });
 
-it("can start a game", () => {
-    const game = require("../src/game.js");
+describe("game logic tests", () => {
+    let game;
+    beforeEach(() => {
+        game = require("../src/game.js");
+    });
+
+    describe("start game", () => {
+        it("can start a game", () => {
+            game.start();
+        });
+
+        it("resets the board on start", () => {
+            game.start();
+            game.placeLetter("x", 1, 1);
+            game.placeLetter("o", 0, 0);
     
-    game.start();
-});
-
-it("can retrieve board state", () => {
-    const game = require("../src/game.js");
-    game.start();
+            game.start();
     
-    const boardState = game.getBoard();
+            const boardState = game.getBoard();
+            for (const column of boardState) {
+                for (const square of column) {
+                    expect(square).toBeNull();
+                }
+            }
+        })
+
+        it("resets the game state on start", () => {
+            game.start();
+            game.placeLetter("x", 0, 0);
     
-    expect(boardState.length).toBe(3);
-    for (const column of boardState) {
-        expect(column.length).toBe(3);
-    }
-});
-
-it("starts with all squares on board empty", () => {
-    const game = require("../src/game.js");
-    game.start();
+            game.start();
     
-    const boardState = game.getBoard();
+            const gameState = game.getState();
+            expect(gameState.turn).toBe("x");
+        })
+    });
 
-    for (const column of boardState) {
-        for (const square of column) {
-            expect(square).toBeNull();
-        }
-    }
+    describe("board state", () => {
+        beforeEach(() => {
+            game.start();
+        });
+
+        it("can retrieve board state", () => {            
+            const boardState = game.getBoard();
+            
+            expect(boardState.length).toBe(3);
+            for (const column of boardState) {
+                expect(column.length).toBe(3);
+            }
+        });
+    
+        it("starts with all squares on board empty", () => {
+            const boardState = game.getBoard();
+    
+            for (const column of boardState) {
+                for (const square of column) {
+                    expect(square).toBeNull();
+                }
+            }
+        });
+    });
+
+    describe("game state", () => {
+        beforeEach(() => {
+            game.start();
+        });
+
+        it("can get the state of the game", () => {
+            const gameState = game.getState();
+
+            expect(gameState.hasOwnProperty('turn')).toBeTruthy();
+            expect(gameState.hasOwnProperty('winner')).toBeTruthy();
+            expect(gameState.turn).toBe("x");
+            expect(gameState.winner).toBe(false);
+        });
+    });
+
+    describe("placing letters", () => {
+        beforeEach(() => {
+            game.start();
+        });
+
+        it("can place a letter 'x'", () => {
+            game.placeLetter("x", 0, 0);
+    
+            const letter = game.getBoard()[0][0];
+            expect(letter).toBe("x");
+        });
+    
+        it("can place a letter 'o'", () => {
+            game.placeLetter("x", 1, 1);
+            
+            game.placeLetter("o", 0, 0);
+    
+            const letter = game.getBoard()[0][0];
+            expect(letter).toBe("o");
+        });
+    
+        it("cannot place an invalid letter", () => {
+            game.placeLetter("z", 0, 0);
+    
+            const letter = game.getBoard()[0][0];
+            expect(letter).toBe(null);
+        });
+    
+        it("cannot place a letter out of bounds", () => {
+            game.placeLetter("o", -1, 3);
+    
+            const boardState = game.getBoard();
+            for (const column of boardState) {
+                for (const square of column) {
+                    expect(square).toBeNull();
+                }
+            }
+        });
+
+        it("cannot replace an existing letter", () => {
+            game.placeLetter("x", 0, 0);
+            game.placeLetter("o", 0, 0);
+    
+            const letter = game.getBoard()[0][0];
+            expect(letter).toBe("x");
+        });
+
+        it("turn changes after placing letter", () => {
+            game.placeLetter("x", 0, 0) ;
+            
+            expect(game.getState().turn).toBe("o");
+        });
+
+        it("ignores moves on the wrong turn", () => {
+            game.placeLetter("o", 0, 0) ;
+            
+            expect(game.getState().turn).toBe("x");
+            const letter = game.getBoard()[0][0];
+            expect(letter).toBe(null);
+        });
+    });
 });
-
-it("can place a letter 'x'", () => {
-    const game = require("../src/game.js");
-    game.start();
-
-    game.placeLetter("x", 0, 0);
-
-    const letter = game.getBoard()[0][0];
-    expect(letter).toBe("x");
-});
-
-it("can place a letter 'o'", () => {
-    const game = require("../src/game.js");
-    game.start();
-
-    game.placeLetter("o", 0, 0);
-
-    const letter = game.getBoard()[0][0];
-    expect(letter).toBe("o");
-});
-
-it("cannot place an invalid letter", () => {
-    const game = require("../src/game.js");
-    game.start();
-
-    game.placeLetter("z", 0, 0);
-
-    const letter = game.getBoard()[0][0];
-    expect(letter).toBe(null);
-});
-
-it("cannot place a letter out of bounds", () => {
-    const game = require("../src/game.js");
-    game.start();
-
-    game.placeLetter("o", -1, 3);
-
-    const boardState = game.getBoard();
-    for (const column of boardState) {
-        for (const square of column) {
-            expect(square).toBeNull();
-        }
-    }
-});
-
-it("cannot replace an existing letter", () => {
-    const game = require("../src/game.js");
-    game.start();
-
-    game.placeLetter("o", 0, 0);
-    game.placeLetter("x", 0, 0);
-
-    const letter = game.getBoard()[0][0];
-    expect(letter).toBe("o");
-});
-
-it("resets the board on start", () => {
-    const game = require("../src/game.js");
-    game.start();
-    game.placeLetter("o", 0, 0);
-    game.placeLetter("x", 1, 1);
-
-    game.start();
-
-    const boardState = game.getBoard();
-    for (const column of boardState) {
-        for (const square of column) {
-            expect(square).toBeNull();
-        }
-    }
-})
